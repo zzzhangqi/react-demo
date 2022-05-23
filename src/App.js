ae7d58d73d5cc29755b30be7e7f8ecbe6f3e63df
zhangqi <13651281168@163.com>
2022-05-23 15:34:32 +0800

    init react demo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="https://smallq.cn/img/logo.png" className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
